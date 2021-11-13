e3ab0337572aafdb9a43111775a339d7324ba82b
Fatih KILIÇ <fkilic1805@gmail.com>
2021-11-13 08:22:22 +0300

    13112021-0822

--- app/src/main/java/com/fatihkilic/muminappandroid/Ayarlar/SesSettingsActivity.java
+++ app/src/main/java/com/fatihkilic/muminappandroid/Ayarlar/SesSettingsActivity.java
@@ -2,7 +2,9 @@ package com.fatihkilic.muminappandroid.Ayarlar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.fatihkilic.muminappandroid.R;
 
@@ -13,4 +15,73 @@ public class SesSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ses_settings);
     }
+
+
+    public void imsakSoundSettingsButton (View view) {
+
+        String vakitInfo = "İmsak Vakti";
+
+        Intent soundSettingIntent = new Intent(SesSettingsActivity.this,SoundSettingsDetailActivity.class);
+        soundSettingIntent.putExtra("VakitInfo",vakitInfo);
+        startActivity(soundSettingIntent);
+
+
+    }
+
+    public void gunesSoundSettingsButton (View view) {
+
+        String vakitInfo = "Güneş Vakti";
+
+        Intent soundSettingIntent = new Intent(SesSettingsActivity.this,SoundSettingsDetailActivity.class);
+        soundSettingIntent.putExtra("VakitInfo",vakitInfo);
+        startActivity(soundSettingIntent);
+
+
+    }
+    public void ogleSoundSettingsButton (View view) {
+
+        String vakitInfo = "Öğle Vakti";
+
+        Intent soundSettingIntent = new Intent(SesSettingsActivity.this,SoundSettingsDetailActivity.class);
+        soundSettingIntent.putExtra("VakitInfo",vakitInfo);
+        startActivity(soundSettingIntent);
+
+
+    }
+
+    public void ikindiSoundSettingsButton (View view) {
+
+        String vakitInfo = "İkindi Vakti";
+
+        Intent soundSettingIntent = new Intent(SesSettingsActivity.this,SoundSettingsDetailActivity.class);
+        soundSettingIntent.putExtra("VakitInfo",vakitInfo);
+        startActivity(soundSettingIntent);
+
+
+    }
+
+    public void aksamSoundSettingsButton (View view) {
+
+        String vakitInfo = "Akşam Vakti";
+
+        Intent soundSettingIntent = new Intent(SesSettingsActivity.this,SoundSettingsDetailActivity.class);
+        soundSettingIntent.putExtra("VakitInfo",vakitInfo);
+        startActivity(soundSettingIntent);
+
+
+    }
+
+    public void yatsiSoundSettingsButton (View view) {
+
+        String vakitInfo = "Yatsı Vakti";
+
+        Intent soundSettingIntent = new Intent(SesSettingsActivity.this,SoundSettingsDetailActivity.class);
+        soundSettingIntent.putExtra("VakitInfo",vakitInfo);
+        startActivity(soundSettingIntent);
+
+
+    }
+
+
+
 }
--- app/src/main/java/com/fatihkilic/muminappandroid/Ayarlar/SoundSettingsDetailActivity.java
+++ app/src/main/java/com/fatihkilic/muminappandroid/Ayarlar/SoundSettingsDetailActivity.java
@@ -2,15 +2,37 @@ package com.fatihkilic.muminappandroid.Ayarlar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
 
 import com.fatihkilic.muminappandroid.R;
+import com.fatihkilic.muminappandroid.databinding.ActivityAyarlarBinding;
+import com.fatihkilic.muminappandroid.databinding.ActivitySoundSettingsDetailBinding;;
 
 public class SoundSettingsDetailActivity extends AppCompatActivity {
+
+    String VakitInfo;
+    private ActivitySoundSettingsDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound_settings_detail);
+
+        binding = ActivitySoundSettingsDetailBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        Intent VakitIntent = getIntent();
+        VakitInfo = VakitIntent.getStringExtra("VakitInfo");
+
+        getSupportActionBar().setTitle(VakitInfo);
+
+
+
+
     }
 }
