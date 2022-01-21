c3eaec3cd98532cac814d829d44f55d7e469c525
FatihPc <fkilic1805@gmail.com>
2022-01-21 18:12:59 +0300

    21012022 - 1812

--- app/src/main/java/com/fatihkilic/muminappandroid/User/MyAccountActivity.java
+++ app/src/main/java/com/fatihkilic/muminappandroid/User/MyAccountActivity.java
@@ -1,16 +1,102 @@
 package com.fatihkilic.muminappandroid.User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.fatihkilic.muminappandroid.R;
+import com.fatihkilic.muminappandroid.databinding.ActivityMyAccountBinding;
+import com.fatihkilic.muminappandroid.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MyAccountActivity extends AppCompatActivity {
+
+    private ActivityMyAccountBinding binding;
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore firebaseFirestore;
+
+    String userName;
+    String name;
+    String surname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_account);
+
+
+        binding = ActivityMyAccountBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        getProfil();
+
+
+
+
+
     }
+
+
+    public void getProfil () {
+
+        String email = auth.getCurrentUser().getEmail();
+
+        DocumentReference usdRef = firebaseFirestore.collection("User").document(email);
+        usdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if (task.isSuccessful()) {
+
+                    DocumentSnapshot document = task.getResult();
+
+                    if (document.exists()) {
+
+                        userName = (String) document.get("userName");
+                        name = (String) document.get("name");
+                        surname = (String) document.get("surName");
+
+                        binding.userNameTextView.setText(userName);
+
+                        StringBuilder nameSurname = new StringBuilder();
+                        nameSurname.append(name);
+                        nameSurname.append(surname);
+
+                        binding.nameTextView.setText(nameSurname.toString());
+
+
+                    } else {
+                        System.out.println("Olumsuz");
+                    }
+
+                } else {
+
+                    System.out.println("cekme başarısız");
+                }
+
+            }
+
+
+        });
+
+
+
+
+
+    }
+
+
+
 }
