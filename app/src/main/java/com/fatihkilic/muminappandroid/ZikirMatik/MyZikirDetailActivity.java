9b762be0a6f92b07903f98a96369f9c402d8ea1a
FatihPc <fkilic1805@gmail.com>
2022-02-19 13:08:26 +0300

    190822 - 1308

--- app/src/main/java/com/fatihkilic/muminappandroid/ZikirMatik/MyZikirDetailActivity.java
+++ app/src/main/java/com/fatihkilic/muminappandroid/ZikirMatik/MyZikirDetailActivity.java
@@ -3,14 +3,71 @@ package com.fatihkilic.muminappandroid.ZikirMatik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 
 import com.fatihkilic.muminappandroid.R;
+import com.fatihkilic.muminappandroid.databinding.ActivityMyZikirDetailBinding;
+import com.fatihkilic.muminappandroid.databinding.ActivityZikirAddBinding;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.onesignal.OneSignal;
 
 public class MyZikirDetailActivity extends AppCompatActivity {
+
+    private ActivityMyZikirDetailBinding binding;
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+
+    private static final String ONESIGNAL_APP_ID = "1966721c-a30c-4299-9d7a-38e084b98072";
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_zikir_detail);
+
+        binding = ActivityMyZikirDetailBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = binding.adView;
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        binding.zikirDuasiTextview.setMovementMethod(new ScrollingMovementMethod());
+
+
+
+
     }
 }
